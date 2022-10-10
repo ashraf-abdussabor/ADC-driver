8ebf4c5423591343b68d480156dedcd51d255bc5
ashraf abdussabor <ashrafabdussabor@gmail.com>
2022-10-10 15:42:25 +0200

    first commit

---
+++ ADC/Debug/temp_sensor_example.d
@@ -0,0 +1,10 @@
+temp_sensor_example.o temp_sensor_example.o: ../temp_sensor_example.c \
+  ../ADC.h ../std_types.h ../lcd.h ../lm35_sensor.h
+
+../ADC.h:
+
+../std_types.h:
+
+../lcd.h:
+
+../lm35_sensor.h:
